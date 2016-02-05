13df9945a9d7502da66764cf8426d3c47d9c15e0
amdewstow <anthonydewstow@Anthony-iZac.local>
2016-02-05 10:57:15 -0600

    error handling

--- poll_load.php
+++ poll_load.php
@@ -1,4 +1,5 @@
 <?
+    session_start();
     include 'vars.php';
     $out = array( );
     foreach ( $servers as $sk => $sv ) {
@@ -17,15 +18,28 @@
             curl_setopt( $curl, CURLOPT_SSL_VERIFYHOST, 0 );
             curl_setopt( $curl, CURLOPT_SSL_VERIFYPEER, 0 );
             curl_setopt( $curl, CURLOPT_RETURNTRANSFER, 1 );
+            curl_setopt( $curl, CURLOPT_CONNECTTIMEOUT, 15 );
             $header[ 0 ] = "Authorization: WHM $whmusername:" . preg_replace( "'(\r|\n)'", "", $hash );
             curl_setopt( $curl, CURLOPT_HTTPHEADER, $header );
             curl_setopt( $curl, CURLOPT_URL, $query );
             $result = curl_exec( $curl );
             if ( $result == false ) {
                 error_log( "curl_exec threw error \"" . curl_error( $curl ) . "\" for $query" );
+                $ar              = array( );
+                $ar[ 'one' ]     = 0;
+                $ar[ 'five' ]    = 10;
+                $ar[ 'fifteen' ] = 15;
+            } else {
+                curl_close( $curl );
+                $ar = json_decode( $result, true );
+                if ( !isset( $ar[ 'one' ] ) ) {
+                    // bad something    
+                    $ar              = array( );
+                    $ar[ 'one' ]     = 15;
+                    $ar[ 'five' ]    = 10;
+                    $ar[ 'fifteen' ] = 0;
+                }
             }
-            curl_close( $curl );
-            $ar                                 = json_decode( $result, true );
             $out[ md5( $sk . ":" . $sv[ 1 ] ) ] = $ar;
         }
     }
